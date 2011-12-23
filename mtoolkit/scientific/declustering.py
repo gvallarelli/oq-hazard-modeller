6bcb7d17c237c79e4bf4c5547fae0947254f9e5e
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2011-12-23 16:10:09 +0100

    declustering.py cleanup, added some docs

--- mtoolkit/scientific/declustering.py
+++ mtoolkit/scientific/declustering.py
@@ -17,53 +17,78 @@
 # version 3 along with MToolkit. If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
-'''Module to implement declustering algorithms'''
+"""
+The purpose of this module is to provide functions
+which implement declustering algorithms. Implemented
+algorithms are:
+- GardnerKnopoff
+"""
 
 import numpy as np
 from catalogue_utilities import decimal_year, haversine
 
 
-# Choose Calculate Magnitude and Distance Windows (for Gardner & Knopoff)
-def calc_windows(m, window_opt):
-    """Function to calculate time and distance windows according \
-    to the methods of "Gruenthal", "Uhrhammer" or "GardnerKnopoff"""
+def calc_windows(magnitude, window_opt):
+    """
+    Allows to calculate distance and time windows (sw, search window)
+    see reference: 'Van Stiphout et al (2010)'.
+    :param magnitude: magnitude
+    :type magnitude: float
+    :param window_opt: window option can be one between: Gruenthal,
+        Uhrhammer, GardnerKnopoff
+    :type window_opt: string
+    :returns: distance and time windows
+    """
+
     if window_opt == 'Gruenthal':
-        # Gruenthal
-        f_space = np.exp(1.77 + np.sqrt(0.037 + 1.02 * m))
-        f_time = np.abs((np.exp(-3.95 + np.sqrt(0.62 + 17.32 * m))) / 365.)
-        f_time[m >= 6.5] = np.power(10, 2.8 + 0.024 * m[m >= 6.5]) / 365.
+        sw_space = np.exp(1.77 + np.sqrt(0.037 + 1.02 * magnitude))
+        sw_time = np.abs(
+            (np.exp(-3.95 + np.sqrt(0.62 + 17.32 * magnitude))) / 365.)
+        sw_time[magnitude >= 6.5] = np.power(
+            10, 2.8 + 0.024 * magnitude[magnitude >= 6.5]) / 365.
+
     elif window_opt == 'Uhrhammer':
-        # Uhrhammer
-        f_space = np.exp(-1.024 + 0.804 * m)
-        f_time = np.exp(-2.87 + 1.235 * m / 365.)
+        sw_space = np.exp(-1.024 + 0.804 * magnitude)
+        sw_time = np.exp(-2.87 + 1.235 * magnitude / 365.)
+
     else:
-        #Gardner & Knopoff
-        f_space = np.power(10.0, 0.1238 * m + 0.983)
-        f_time = np.power(10.0, 0.032 * m + 2.7389) / 365.
-        f_time[m < 6.5] = np.power(10.0, 0.5409 * m[m < 6.5] - 0.547) / 365.
-    return f_space, f_time
+        sw_space = np.power(10.0, 0.1238 * magnitude + 0.983)
+        sw_time = np.power(10.0, 0.032 * magnitude + 2.7389) / 365.
+        sw_time[magnitude < 6.5] = np.power(
+            10.0, 0.5409 * magnitude[magnitude < 6.5] - 0.547) / 365.
+
+    return sw_space, sw_time
 
 
 def gardner_knopoff_decluster(
-    data, window_opt='GardnerKnopoff', fs_time_prop=0):
-    ''' Function to implement Gardner & Knopoff Declustering Algorithm
-        data = EQ catalogue in sample format
-        WindowOpt = 'GardnerKnopoff' for Gardner & Knopoff windows
-                              'Uhrhammer' for 'Uhrhammer' implementation
-                              'Gruenthal' for Gruenthal implementation
-        FSTimeProp = Foreshock time window as a proportion of aftershock
-                              time window '''
-
-    #~ #Define reference ellipsoid for geospatial calculations
-    #~ ref_geoid = Geod(ellps="WGS84")
+    catalog_matrix, window_opt='GardnerKnopoff', fs_time_prop=0):
+    """
+    Gardner Knopoff algorithm
+    :param catalog_matrix: eq catalog in a matrix format with
+        these coloumns in order: 'year', 'month', 'day', 'longitude',
+        'latitude', 'Mw'
+    :type catalog_matrix: numpy.ndarray
+    :keyword window_opt: method used in calculating distance and time
+        windows
+    :type window_opt: string
+    :keyword fs_time_prop: foreshock time window as a proportion of
+        aftershock time window
+    :type fs_time_prop: positive float
+    :returns: vcl vector indicating cluster number,
+        vmain_shock catalog containing non-clustered events,
+        flag_vector vector indicating which eq events belong to
+        a cluster
+    :rtype: numpy.ndarray
+    """
 
     # Get relevent parameters
-    m = data[:, 5]
-    neq = np.shape(data)[0]  # Number of earthquakes
+    m = catalog_matrix[:, 5]
+    neq = np.shape(catalog_matrix)[0]  # Number of earthquakes
     # Get decimal year (needed for time windows)
-    year_dec = decimal_year(data[:, 0], data[:, 1], data[:, 2])
+    year_dec = decimal_year(
+        catalog_matrix[:, 0], catalog_matrix[:, 1], catalog_matrix[:, 2])
     # Get space and time windows corresponding to each event
-    f_space, f_time = calc_windows(m, window_opt)
+    sw_space, sw_time = calc_windows(m, window_opt)
     eqid = np.arange(0, neq, 1)  # Initial Position Identifier
 
     # Pre-allocate cluster index vectors
@@ -72,9 +97,9 @@ def gardner_knopoff_decluster(
     # Sort magnitudes into descending order
     id0 = np.flipud(np.argsort(m, kind='heapsort'))
     m = m[id0]
-    data = data[id0, :]
-    f_space = f_space[id0]
-    f_time = f_time[id0]
+    catalog_matrix = catalog_matrix[id0, :]
+    sw_space = sw_space[id0]
+    sw_time = sw_time[id0]
     year_dec = year_dec[id0]
     eqid = eqid[id0]
     #Begin cluster identification
@@ -82,17 +107,18 @@ def gardner_knopoff_decluster(
     while i < neq:
         if vcl[i] == 0:
             #fMag = m[i]
-            #mPos = data[i, 10:11]
+            #mPos = catalog_matrix[i, 10:11]
 
             # Find Events inside both fore- and aftershock time windows
             dt = year_dec - year_dec[i]
             ick = np.zeros(neq, dtype=int)
-            vsel = np.logical_and(dt >= (-f_time[i] * fs_time_prop),
-                                              dt <= f_time[i], vcl == 0)
+            vsel = np.logical_and(dt >= (-sw_time[i] * fs_time_prop),
+                                              dt <= sw_time[i], vcl == 0)
             # Of those events inside time window, find those inside distance
             # window
-            vsel1 = haversine(data[vsel, 3], data[vsel, 4], data[i, 3],
-                               data[i, 4]) <= f_space[i]
+            vsel1 = haversine(
+                catalog_matrix[vsel, 3], catalog_matrix[vsel, 4],
+                catalog_matrix[i, 3], catalog_matrix[i, 4]) <= sw_space[i]
             # Update logical array so that those events inside time window
             # but outside distance window are switched to False
             vsel[vsel] = vsel1
@@ -115,13 +141,13 @@ def gardner_knopoff_decluster(
             # Already allocated to cluster - skip event
             i += 1
 
-    # Re-sort the data into original order
+    # Re-sort the catalog_matrix into original order
     id1 = np.argsort(eqid, kind='heapsort')
     eqid = eqid[id1]
-    data = data[id1, :]
+    catalog_matrix = catalog_matrix[id1, :]
     vcl = vcl[id1]
     # Now to produce a catalogue with aftershocks purged
-    vmain_shock = data[np.nonzero(vcl == 0)[0], :]
+    vmain_shock = catalog_matrix[np.nonzero(vcl == 0)[0], :]
     # Also create a simple flag vector which, for each event, takes
     # a value of 1 if aftershock, -1 if foreshock, and 0 otherwise
     flagvector = np.copy(vcl)
