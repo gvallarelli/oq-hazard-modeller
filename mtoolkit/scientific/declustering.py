ec20a7e5e0ed164722f2044e257b47518bd3b342
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2012-01-31 11:35:00 +0100

    extracted 2 methods find aftershocks and foreshocks

--- mtoolkit/scientific/declustering.py
+++ mtoolkit/scientific/declustering.py
@@ -162,6 +162,72 @@ def gardner_knopoff_decluster(
     return vcl, vmain_shock, flagvector
 
 
+def _find_aftershocks(dtime, nval, time_window):
+    """
+    Searches for aftershocks within the moving
+    time window
+    :param dtime: time since main event
+    :type dtime: numpy.ndarray
+    :param nval: number of events in search window
+    :type nval: int
+    :param time_window: Length (in days) of moving time window
+    :type time_window: positive float
+    :returns: **vsel** index vector for aftershocks
+    :rtype: numpy.ndarray
+    """
+
+    vsel = np.array(np.ones(nval), dtype=bool)
+    initval = dtime[0]  # Start with the mainshock
+
+    j = 1
+    while j < nval:
+        ddt = dtime[j] - initval
+        # Is event after previous event and within time window?
+        vsel[j] = np.logical_and(ddt >= 0.0, ddt <= time_window)
+        if vsel[j]:
+            # Reset time window to new event time
+            initval = dtime[j]
+        j += 1
+    return vsel
+
+
+def _find_foreshocks(dtime, nval, time_window, vsel_aftershocks):
+    """
+    Searches for foreshocks within the moving
+    time window
+    :param dtime: time since main event
+    :type dtime: numpy.ndarray
+    :param nval: number of events in search window
+    :type nval: int
+    :param time_window: Length (in days) of moving time window
+    :type time_window: positive float
+    :param vsel_aftershocks: index vector for aftershocks
+    :type vsel_aftershocks: numpy.ndarray
+    :returns: **vsel** index vector for foreshocks
+    :rtype: numpy.ndarray
+    """
+
+    j = 1
+    vsel = np.array(np.zeros(nval), dtype=bool)
+    initval = dtime[0]
+
+    while j < nval:
+        if vsel_aftershocks[j]:
+        # Event already allocated as an aftershock - skip
+            j += 1
+        else:
+            ddt = dtime[j] - initval
+            # Is event before previous event and within time window?
+            vsel[j] = np.logical_and(ddt <= 0.0,
+                                      ddt >= -(time_window))
+            if vsel[j]:
+            # Yes, reset time window to new event
+                initval = dtime[j]
+        j += 1
+
+    return vsel
+
+
 def afteran_decluster(
     catalogue_matrix, window_opt='GardnerKnopoff', time_window=60.):
     '''AFTERAN declustering algorithm.
@@ -224,36 +290,10 @@ def afteran_decluster(
 
             nval = np.shape(dtime)[0]  # Number of events inside valid window
             # Pre-allocate boolean array (all True)
-            vsel1 = np.array(np.ones(nval), dtype=bool)
-            initval = dtime[0]  # Start with the mainshock
-            # Search for aftershocks
-            j = 1
-            while j < nval:
-                ddt = dtime[j] - initval
-                # Is event after previous event and within time window?
-                vsel1[j] = np.logical_and(ddt >= 0.0, ddt <= time_window)
-                if vsel1[j]:
-                    # Reset time window to new event time
-                    initval = dtime[j]
-                j += 1
 
-            # Now search for foreshocks
-            j = 1
-            vsel2 = np.array(np.zeros(nval), dtype=bool)
-            initval = dtime[0]
-            while j < nval:
-                if vsel1[j]:
-                    # Event already allocated as an aftershock - skip
-                    j += 1
-                else:
-                    ddt = dtime[j] - initval
-                    # Is event before previous event and within time window?
-                    vsel2[j] = np.logical_and(ddt <= 0.0,
-                                              ddt >= -(time_window))
-                    if vsel2[j]:
-                        # Yes, reset time window to new event
-                        initval = dtime[j]
-                    j += 1
+            vsel1 = _find_aftershocks(dtime, nval, time_window)
+            vsel2 = _find_foreshocks(dtime, nval, time_window, vsel1)
+
             temp_vsel = np.copy(vsel)
             temp_vsel[vsel] = np.logical_or(vsel1, vsel2)
             if np.shape(np.nonzero(temp_vsel)[0])[0] > 1:
