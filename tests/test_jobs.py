eaf45b9d3cb2b71a2a40d790da8018fde3b70e39
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2012-02-07 11:47:56 +0100

    final touches refactored test_store, removed unuseful dependencies

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -25,8 +25,9 @@ import filecmp
 
 import unittest
 
+from tests.helper import create_context
 
-from tests.helper import create_workflow, create_context, run
+from mtoolkit.eqcatalog import EqEntryReader
 
 from mtoolkit.source_model import (AreaSource, AREA_BOUNDARY, POINT,
                                     TRUNCATED_GUTEN_RICHTER,
@@ -45,13 +46,18 @@ from nrml.nrml_xml import get_data_path, DATA_DIR
 RUPTURE_KEY = 'rupture_rate_model'
 
 
+def acquire_catalog(pathname):
+    eq_catalog = []
+    reader = EqEntryReader(pathname)
+    for eq_entry in reader.read():
+        eq_catalog.append(eq_entry)
+    return eq_catalog
+
+
 class JobsTestCase(unittest.TestCase):
 
     def setUp(self):
         self.context_jobs = create_context('config_jobs.yml')
-
-        self.preprocessing_config = get_data_path(
-            'config_preprocessing.yml', DATA_DIR)
 
         self.expected_preprocessed_catalogue = get_data_path(
             'expected_preprocessed_catalogue.csv', DATA_DIR)
@@ -65,10 +71,10 @@ class JobsTestCase(unittest.TestCase):
                 'Identifier': 20000102034913, 'day': 2, 'minute': 49,
                 'hour': 3, 'mb': '', 'SemiMinor90': 1.01, 'longitude': 7.282,
                 'depth': 9.3, 'ML': 1.7, 'sigmaML': 0.1}
-
         read_eq_catalog(self.context_jobs)
 
         self.assertEqual(10, len(self.context_jobs.eq_catalog))
+
         self.assertEqual(expected_first_eq_entry,
                 self.context_jobs.eq_catalog[0])
 
@@ -79,7 +85,6 @@ class JobsTestCase(unittest.TestCase):
         asource.area_source_id = "src03"
         asource.name = "Quito"
         asource.tectonic_region = "Active Shallow Crust"
-
         area_boundary = [-122.5, 37.5, -121.5,
                             37.5, -121.5, 38.5,
                             -122.5, 38.5]
@@ -87,32 +92,26 @@ class JobsTestCase(unittest.TestCase):
                 [POINT(area_boundary[i],
                     area_boundary[i + 1])
                     for i in xrange(0, len(area_boundary), 2)])
-
         truncated_gutenberg_richter = TRUNCATED_GUTEN_RICHTER(
             5.0, 0.8, 5.0, 7.0, "ML")
-
         strike = 0.0
         dip = 90.0
         rake = 0.0
-
         asource.rupture_rate_model = RUPTURE_RATE_MODEL(
             truncated_gutenberg_richter, strike, dip, rake)
-
         magnitude = MAGNITUDE("ML", [6.0, 6.5, 7.0])
         depth = [5000.0, 3000.0, 0.0]
-
         asource.rupture_depth_dist = RUPTURE_DEPTH_DISTRIB(magnitude, depth)
-
         asource.hypocentral_depth = 5000.0
-
         read_source_model(self.context_jobs)
+
         self.assertEqual(1, len(self.context_jobs.sm_definitions))
+
         self.assertEqual(asource,
                 self.context_jobs.sm_definitions[0])
 
     def test_create_default_source_model(self):
         default_as = [default_area_source()]
-
         create_default_source_model(self.context_jobs)
 
         self.assertEqual(default_as, self.context_jobs.sm_definitions)
@@ -120,22 +119,24 @@ class JobsTestCase(unittest.TestCase):
     def test_parameters_gardner_knopoff(self):
         mocked_func = Mock(return_value=([], [], []))
         self.context_jobs.map_sc['gardner_knopoff'] = mocked_func
-
         gardner_knopoff(self.context_jobs)
 
         self.assertTrue(mocked_func.called)
+
         mocked_func.assert_called_with(None, 'GardnerKnopoff', 0.5)
+
         self.assertRaises(AssertionError, mocked_func.assert_called_with,
             'Uhrhammer', 0.1)
 
     def test_parameters_afteran(self):
         mocked_func = Mock(return_value=([], [], []))
         self.context_jobs.map_sc['afteran'] = mocked_func
-
         afteran(self.context_jobs)
 
         self.assertTrue(mocked_func.called)
+
         mocked_func.assert_called_with(None, 'Uhrhammer', 150.8)
+
         self.assertRaises(AssertionError, mocked_func.assert_called_with,
             'Gruenthal', 3.1)
 
@@ -143,7 +144,6 @@ class JobsTestCase(unittest.TestCase):
         self.context_jobs.working_catalog = np.array([[1, 2, 3, 4, 5, 6]])
         mocked_func = Mock()
         self.context_jobs.map_sc['stepp'] = mocked_func
-
         stepp(self.context_jobs)
 
         self.assertTrue(mocked_func.called)
@@ -163,7 +163,6 @@ class JobsTestCase(unittest.TestCase):
         self.context_jobs.cur_sm = Mock()
         mocked_func = Mock(return_value=(0, 0, 0, 0))
         self.context_jobs.map_sc['recurrence'] = mocked_func
-
         recurrence(self.context_jobs)
 
         self.assertTrue(mocked_func.called)
@@ -180,10 +179,12 @@ class JobsTestCase(unittest.TestCase):
             None, 3.4, 'Graeme', 0.5, 7.6)
 
     def test_store_catalog_in_csv_after_preprocessing(self):
-        context = create_context(self.preprocessing_config)
-        workflow = create_workflow(context.config)
-        run(workflow, context)
-        store_preprocessed_catalog(context)
+        self.context_jobs.selected_eq_vector = np.array(
+            [0, 0, 0, 1, 1, 0, 1, 0, 1, 0])
+        self.context_jobs.eq_catalog = acquire_catalog(
+            self.context_jobs.config['eq_catalog_file'])
+        self.context_jobs.catalog_matrix = self.context_jobs.eq_catalog
+        store_preprocessed_catalog(self.context_jobs)
 
         self.assertTrue(filecmp.cmp(self.expected_preprocessed_catalogue,
-                context.config['pprocessing_result_file']))
+                self.context_jobs.config['pprocessing_result_file']))
